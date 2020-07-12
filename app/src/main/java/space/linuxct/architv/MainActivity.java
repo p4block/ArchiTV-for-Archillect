0a531ed99b7695c4e067b0fdfa8c509456a212fb
Pablo <42.pablo.ms@gmail.com>
2020-07-12 20:02:02 +0200

    Leaning

--- app/src/main/java/space/linuxct/architv/MainActivity.java
+++ app/src/main/java/space/linuxct/architv/MainActivity.java
@@ -9,49 +9,22 @@ import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
-    public WebView backgroundView;
-
-    String url;
-
-    private Runnable mRunnable;
-
-    public static boolean donette = false;
-
-    PowerManager.WakeLock wakeLock;
-
+    private WebView mWebView;
+    private String upstreamURL = "https://archillect.com/tv";
+    private PowerManager.WakeLock wakeLock;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        backgroundView = findViewById(R.id.backgroundview);
+        mWebView = findViewById(R.id.backgroundview);
 
-        backgroundView.setWebViewClient(new WebViewClient(){});
+        mWebView.setWebViewClient(new WebViewClient(){});
 
         PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
         wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Archillect:wakelock");
-        wakeLock.acquire();
-
-        url = "https://archillect.com/tv";
-        backgroundView.loadUrl(url);
-
-        mRunnable = new Runnable() {
-            @Override
-            public void run() {
-                if (!donette) {
-                    donette = true;
-                    backgroundView.postDelayed(this, 7000);
-                    WebSettings webSettings = backgroundView.getSettings();
-                    webSettings.setJavaScriptEnabled(true);
-                    backgroundView.loadUrl(url);
-
-                }
-            }
-        };
-        backgroundView.postDelayed(mRunnable,7000);
     }
-
 
     @Override
     public void onResume() {
@@ -60,6 +33,11 @@ public class MainActivity extends Activity {
                 wakeLock.acquire();
             }
         }
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl(upstreamURL);
+
         super.onResume();
     }
 
